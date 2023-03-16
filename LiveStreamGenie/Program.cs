52244e7c2bcaea61289f71b76b86e3fa458b3fd9
Micheal Colhoun <micheal@colhountech.com>
2023-03-16 21:19:21 +0000

    Simple Version Working

--- LiveStreamGenie/Program.cs
+++ LiveStreamGenie/Program.cs
@@ -2,16 +2,46 @@ namespace LiveStreamGenie
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+        private static NotifyIcon notifyIcon;
+        private static ContextMenuStrip cms;
+        private static ApplicationContext context;
+
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new Icon("favicon.ico");
+            notifyIcon.Text = "Genie";
+
+            cms = new ContextMenuStrip();
+
+            cms.Items.Add(new ToolStripMenuItem("Reconnect", null, new EventHandler(Reconnect_Click)));
+            cms.Items.Add(new ToolStripSeparator());
+            cms.Items.Add(new ToolStripMenuItem("Quit", null, new EventHandler(Quit_Click), "Quit"));
+
+            notifyIcon.ContextMenuStrip = cms;
+            notifyIcon.Visible = true;
+
+            // Create an ApplicationContext and run a message loop
+            // on the context.
+            context = new ApplicationContext();
+            Application.Run(context);
+
+            // Hide notify icon on quit
+            notifyIcon.Visible = false;
+        }
+
+        static void Reconnect_Click(object? sender, System.EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+
+        static void Quit_Click(object? sender, System.EventArgs e)
+        {
+            // End application though ApplicationContext
+            context.ExitThread();
         }
     }
 }
