931250aec4f220a640c5ec98406ebd815e721d79
Micheal Colhoun <micheal@colhountech.com>
2023-03-17 14:39:13 +0000

    Updated the Icons for both forms to the Live Streaming Genie app

--- LiveStreamGenie/AboutForm.cs
+++ LiveStreamGenie/AboutForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiveStreamGenie.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace LiveStreamGenie
         public AboutForm()
         {
             InitializeComponent();
+            Icon = Resources.favicon;
         }
     }
 }
