13d8b81112916bd4bdbd6ea5d23bfe7fba5ffec7
Micheal Colhoun <micheal@colhountech.com>
2023-03-17 10:41:55 +0000

    Added MyApplicationContext and 2 dummy forms and to only quit when both forms are closed (or exit from systray)

--- LiveStreamGenie/Program.cs
+++ LiveStreamGenie/Program.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace LiveStreamGenie
 {
@@ -7,61 +9,243 @@ namespace LiveStreamGenie
         private static NotifyIcon notifyIcon;
         private static ContextMenuStrip cms;
         private static ApplicationContext context;
-        private static System.Timers.Timer timer;   
+        private static System.Timers.Timer timer;
 
-        [STAThread]
-        static void Main()
+        class MyApplicationContext : ApplicationContext
         {
-            ApplicationConfiguration.Initialize();
+            private int _formCount;
+            private SettingsForm _form1;
+            private AboutForm _form2;
 
-            notifyIcon = new NotifyIcon();
-            notifyIcon.Icon = new Icon("favicon.ico");
-            notifyIcon.Text = "Genie";
+            private Rectangle _form1Position;
+            private Rectangle _form2Position;
 
-            cms = new ContextMenuStrip();
-
-            cms.Items.Add(new ToolStripMenuItem("Reconnect", null, new EventHandler(Reconnect_Click)));
-            cms.Items.Add(new ToolStripSeparator());
-            cms.Items.Add(new ToolStripMenuItem("Quit", null, new EventHandler(Quit_Click), "Quit"));
-
-            notifyIcon.ContextMenuStrip = cms;
-            notifyIcon.Visible = true;
-            
+            private FileStream _userData;
 
 
-            // Set up the timer
-            timer = new System.Timers.Timer(10 * 1000); // 60 seconds
-            timer.Elapsed += Timer_Elapsed;
-            timer.Start();
+            private MyApplicationContext()
+            {
+                _formCount = 0;
+
+                // Handle the ApplicationExit event to know when the application is exiting.
+                Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
+
+                try
+                {
+                    // Create a file that the application will store user specific data in.
+                    _userData = new FileStream(Application.UserAppDataPath + "\\appdata.txt", FileMode.OpenOrCreate);
+                }
+                catch (IOException e)
+                {
+                    // Inform the user that an error occurred.
+                    MessageBox.Show("An error occurred while attempting to show the application." +
+                                    "The error is:" + e.ToString());
+
+                    // Exit the current thread instead of showing the windows.
+                    ExitThread();
+                }
+
+                // Create both application forms and handle the Closed event
+                // to know when both forms are closed.
+                _form1 = new SettingsForm();
+                _form1.Closed += new EventHandler(OnFormClosed);
+                _form1.Closing += new CancelEventHandler(OnFormClosing);
+                _formCount++;
+
+                _form2 = new AboutForm();
+                _form2.Closed += new EventHandler(OnFormClosed);
+                _form2.Closing += new CancelEventHandler(OnFormClosing);
+                _formCount++;
+
+                // Get the form positions based upon the user specific data.
+                if (ReadFormDataFromFile())
+                {
+                    // If the data was read from the file, set the form
+                    // positions manually.
+                    _form1.StartPosition = FormStartPosition.Manual;
+                    _form2.StartPosition = FormStartPosition.Manual;
+
+                    _form1.Bounds = _form1Position;
+                    _form2.Bounds = _form2Position;
+                }
+
+                // Show both forms.
+                _form1.Show();
+                _form2.Show();
+            }
+
+            private void OnApplicationExit(object sender, EventArgs e)
+            {
+                // When the application is exiting, write the application data to the
+                // user file and close it.
+                WriteFormDataToFile();
+
+                try
+                {
+                    // Ignore any errors that might occur while closing the file handle.
+                    _userData.Close();
+                }
+                catch { }
+            }
+
+            private void OnFormClosing(object sender, CancelEventArgs e)
+            {
+                // When a form is closing, remember the form position so it
+                // can be saved in the user data file.
+                if (sender is SettingsForm)
+                    _form1Position = ((Form)sender).Bounds;
+                else if (sender is AboutForm)
+                    _form2Position = ((Form)sender).Bounds;
+            }
+
+            private void OnFormClosed(object sender, EventArgs e)
+            {
+                // When a form is closed, decrement the count of open forms.
+
+                // When the count gets to 0, exit the app by calling
+                // ExitThread().
+                _formCount--;
+                if (_formCount == 0)
+                {
+                    ExitThread();
+                }
+            }
+
+            private bool WriteFormDataToFile()
+            {
+                // Write the form positions to the file.
+                UTF8Encoding encoding = new UTF8Encoding();
+
+                RectangleConverter rectConv = new RectangleConverter();
+                string form1pos = rectConv.ConvertToString(_form1Position);
+                string form2pos = rectConv.ConvertToString(_form2Position);
+
+                byte[] dataToWrite = encoding.GetBytes("~" + form1pos + "~" + form2pos);
+
+                try
+                {
+                    // Set the write position to the start of the file and write
+                    _userData.Seek(0, SeekOrigin.Begin);
+                    _userData.Write(dataToWrite, 0, dataToWrite.Length);
+                    _userData.Flush();
+
+                    _userData.SetLength(dataToWrite.Length);
+                    return true;
+                }
+                catch
+                {
+                    // An error occurred while attempting to write, return false.
+                    return false;
+                }
+            }
+
+            private bool ReadFormDataFromFile()
+            {
+                // Read the form positions from the file.
+                UTF8Encoding encoding = new UTF8Encoding();
+                string data;
+
+                if (_userData.Length != 0)
+                {
+                    byte[] dataToRead = new byte[_userData.Length];
+
+                    try
+                    {
+                        // Set the read position to the start of the file and read.
+                        _userData.Seek(0, SeekOrigin.Begin);
+                        _userData.Read(dataToRead, 0, dataToRead.Length);
+                    }
+                    catch (IOException e)
+                    {
+                        string errorInfo = e.ToString();
+                        // An error occurred while attempt to read, return false.
+                        return false;
+                    }
+
+                    // Parse out the data to get the window rectangles
+                    data = encoding.GetString(dataToRead);
+
+                    try
+                    {
+                        // Convert the string data to rectangles
+                        RectangleConverter rectConv = new RectangleConverter();
+                        string form1pos = data.Substring(1, data.IndexOf("~", 1) - 1);
+
+                        _form1Position = (Rectangle)rectConv.ConvertFromString(form1pos);
+
+                        string form2pos = data.Substring(data.IndexOf("~", 1) + 1);
+                        _form2Position = (Rectangle)rectConv.ConvertFromString(form2pos);
+
+                        return true;
+                    }
+                    catch
+                    {
+                        // Error occurred while attempting to convert the rectangle data.
+                        // Return false to use default values.
+                        return false;
+                    }
+                }
+                else
+                {
+                    // No data in the file, return false to use default values.
+                    return false;
+                }
+            }
+            [STAThread]
+            static void Main()
+            {
+
+                ApplicationConfiguration.Initialize();
+
+                notifyIcon = new NotifyIcon();
+                notifyIcon.Icon = new Icon("favicon.ico");
+                notifyIcon.Text = "Genie";
+
+                cms = new ContextMenuStrip();
+
+                cms.Items.Add(new ToolStripMenuItem("Reconnect", null, new EventHandler(Reconnect_Click)));
+                cms.Items.Add(new ToolStripSeparator());
+                cms.Items.Add(new ToolStripMenuItem("Quit", null, new EventHandler(Quit_Click), "Quit"));
+
+                notifyIcon.ContextMenuStrip = cms;
+                notifyIcon.Visible = true;
 
 
 
-            // Create an ApplicationContext and run a message loop
-            // on the context.
-            context = new ApplicationContext();
-            Application.Run(context);
-
-            // Hide notify icon on quit
-            notifyIcon.Visible = false;
-        }
-        private static void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
+                // Set up the timer
+                timer = new System.Timers.Timer(10 * 1000); // 60 seconds
+                timer.Elapsed += Timer_Elapsed;
+                timer.Start();
 
 
-            // Show the message
-            notifyIcon.ShowBalloonTip(3000, "Title", "Message", ToolTipIcon.Info);
 
-        }
+                // Create an ApplicationContext and run a message loop
+                // on the context.
+                context = new MyApplicationContext();
+                Application.Run(context);
 
-        static void Reconnect_Click(object? sender, System.EventArgs e)
-        {
-            MessageBox.Show("Hello World!");
-        }
+                // Hide notify icon on quit
+                notifyIcon.Visible = false;
+            }
+            private static void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+            {
 
-        static void Quit_Click(object? sender, System.EventArgs e)
-        {
-            // End application though ApplicationContext
-            context.ExitThread();
+
+                // Show the message
+                notifyIcon.ShowBalloonTip(3000, "Title", "Message", ToolTipIcon.Info);
+
+            }
+
+            static void Reconnect_Click(object? sender, System.EventArgs e)
+            {
+                MessageBox.Show("Hello World!");
+            }
+
+            static void Quit_Click(object? sender, System.EventArgs e)
+            {
+                // End application though ApplicationContext
+                context.ExitThread();
+            }
         }
     }
 }
--- LiveStreamGenie/README.md
+++ LiveStreamGenie/README.md
@@ -1,3 +1,24 @@
 #
  
-Make sure you turn off nofication sounds or it will beep everyt time you get a notification
+Make sure you turn off nofication sounds or it will beep every time you get a notification
+
+
+![](./turn-off-sounds.png)
+
+
+
+# Technical Design
+
+This C# code creates an instance of the ApplicationContext class and starts a message loop by calling the Application.Run method, which runs the message loop on the ApplicationContext instance.
+
+An ApplicationContext is a class that manages the lifetime of a Windows Forms application. It provides a context in which forms can be shown and closed, and allows you to define application-level behavior, such as handling unhandled exceptions and controlling the shutdown of the application.
+
+The Application.Run method starts a message loop that processes messages from the operating system and dispatches them to the appropriate forms and controls in the application. This message loop is responsible for handling user input, redrawing the user interface, and executing timers and other asynchronous operations.
+
+Overall, this code is a standard way to start a Windows Forms application and run its message loop, which enables the application to handle user input and interact with the operating system.
+
+
+
+
+
+Regenerate response
